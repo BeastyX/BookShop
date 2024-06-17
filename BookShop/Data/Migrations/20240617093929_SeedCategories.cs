0ac5c2f37ebe025e781823b97c635269f2a49cd5
Luka Lazarevic <lukalazarevic001@gmail.com>
2024-06-17 11:59:53 +0200

    Seedovao Kategorije

---
+++ BookShop/Data/Migrations/20240617093929_SeedCategories.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookShop.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData
+                (
+                    table: "Categories",
+                    column: "Name",
+                    values: ["Akcija", "Drama", "Misterija", "Poezija", "Triler", "Istorija"]
+                );
+
+            //migrationBuilder.Sql("INSERT INTO Categories(Name) VALUES('Akcija')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData
+                (
+                    table: "Categories",
+                    keyColumn: "Name",
+                    keyValues: ["Akcija", "Drama", "Misterija", "Poezija", "Triler", "Istorija"]
+                );
+        }
+    }
+}
