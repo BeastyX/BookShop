9d3d7e74b3ebdd73e0559e36386e1c7d0932a875
Luka Lazarevic <lukalazarevic001@gmail.com>
2024-06-17 12:17:25 +0200

    Dodao sample categories kontroler

---
+++ BookShop/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,16 @@
+﻿using BookShop.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class CategoriesController : Controller
+{
+    private ApplicationDbContext _context;
+    public CategoriesController(ApplicationDbContext context) => _context = context;
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
